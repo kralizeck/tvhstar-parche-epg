ecf58a823ebda66cf0b0353986a2543a18192265
Fernando Hernández <er.tesnico@gmail.com>
2018-08-23 12:50:05 +0200

    Primera versión completa

--- parche-epg.sh
+++ parche-epg.sh
@@ -5,8 +5,11 @@
 
 # parámetro recibido
 dias=$1
+
+# variables
+ficheroXML='/tmp/guia.movistar-xml-TMP.xml' # fichero de destino
 sdias=86400 # segundos por día
-dias=$((dias*sdias)) # días pasados a segundos
+dias=$((dias*sdias)) # días pedidos pasados a segundos
 desde=`date "+%s"` # fecha actual en formato epoch
 hasta=$((desde+dias))
 
@@ -17,14 +20,17 @@ hasta=`date -j -r $hasta +"%Y-%m-%d"`
 #edición de data.txt con las nuevas fechas desde y hasta
 perl -p -i'.bak' -e "s/&export-date-from=\d{4}-\d{2}-\d{2}/&export-date-from=$desde/; s/&export-date-to=\d{4}-\d{2}-\d{2}/&export-date-to=$hasta/;" data.txt
 
-# borro los archivos de guía viejos
-rm -rf /tmp/guia.movistar-xml*
+echo "\nSolicitando la guía desde el $desde al $hasta...\n"
 
 # descarga del xml de movistar. Todos los canales excepto alquiler
-time curl 'http://comunicacion.movistarplus.es/wp-admin/admin-post.php' -H 'Connection: keep-alive' -H 'Cache-Control: max-age=0' -H 'Origin: http://comunicacion.movistarplus.es' -H 'Upgrade-Insecure-Requests: 1' -H 'Content-Type: application/x-www-form-urlencoded' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'Referer: http://comunicacion.movistarplus.es/programacion/' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: es,en;q=0.9,es-ES;q=0.8' --data "@data.txt" --compressed -o /tmp/guia.movistar-xml.xml
+curl 'http://comunicacion.movistarplus.es/wp-admin/admin-post.php' -H 'Connection: keep-alive' -H 'Cache-Control: max-age=0' -H 'Origin: http://comunicacion.movistarplus.es' -H 'Upgrade-Insecure-Requests: 1' -H 'Content-Type: application/x-www-form-urlencoded' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'Referer: http://comunicacion.movistarplus.es/programacion/' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: es,en;q=0.9,es-ES;q=0.8' --data "@data.txt" --compressed -o $ficheroXML
+
+echo -e "\nConvirtiendo $ficheroXML a formato xml viejuno de exportación de movistar...\n"
 
 # edición del xml recibido para que sea igual que en la antigua exportación de movistar
-perl -p -i'.bak' -e "s/<\?xml version=\"1.0\" encoding=\"utf-8\"\?><xml>/<\?xml version='1.0' encoding='UTF-8' \?>/; s/<\/xml>//g; s/(<export>)/\n        \t\t\t\1/g; s/(<\/pase>)/<web><\/web>\n\t\t\t\t\t\1/g; s/ {24}/\t\t\t\t\t/g; s/&/&amp;/g; s/cadena=\"(.*?)\" fecha=\">(.*?)\"\>/cadena='\1' fecha='\2'>/g;" /tmp/guia.movistar-xml.xml
+perl -p -i'.bak' -e "s/<\?xml version=\"1.0\" encoding=\"utf-8\"\?><xml>/<\?xml version='1.0' encoding='UTF-8' \?>/; s/<\/xml>//g; s/(<export>)/\n        \t\t\t\1/g; s/(<\/pase>)/<web><\/web>\n\t\t\t\t\t\1/g; s/ {24}/\t\t\t\t\t/g; s/&/&amp;/g; s/cadena=\"(.*?)\" fecha=\">(.*?)\"\>/cadena='\1' fecha='\2'>/g;" $ficheroXML
 
-# lo paso por tvhstar.sh, pero con src/server.js editado para que no descargue la guía (usará el preparado anteriormente) y no se quede en ejecución programada
+node borra_duplis_y_ordena.js $ficheroXML
+
+# lanzo tvhstar.sh (el ficheroXML es el mismo que el definido en server.js), pero con src/server.js editado para que no descargue la guía (usará el preparado anteriormente) y no se quede en ejecución programada
 ./tvhstar.sh
